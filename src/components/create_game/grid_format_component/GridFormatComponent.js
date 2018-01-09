38b77f12c164c6a8db3e7f1a6baad77f958a33e4
AleksB <aleksb@localhost.localdomain>
2018-01-09 20:16:12 +0200

    just playing with redux

--- src/components/create_game/grid_format_component/GridFormatComponent.js
+++ src/components/create_game/grid_format_component/GridFormatComponent.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default ({ formats }) => (
+export default ({ formats, gameConfig, action }) => (
   <div>
     {
-      Object.values(formats).map((form) => <div key={form}>{`${form} X ${form}`}</div>)
+      Object.values(formats).map(
+        (form) => <div key={form} onClick={() => action(form)}>{`${form} X ${form} current : ${gameConfig.gridFormat}`}</div>)
     }
   </div>
 )
--- src/components/create_game/grid_format_component/GridFormatContainer.js
+++ src/components/create_game/grid_format_component/GridFormatContainer.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import GridFormatComponent from './GridFormatComponent'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
+import GameConfigModule from '../../../modules/game_config'
 import { formats } from '../../../config'
+
+const { selectors, actions } = GameConfigModule
 
 class GridFormatContainer extends Component {
 
   render() {
-    return <GridFormatComponent formats={formats} />
+    const { gameConfig, setGridFormat } = this.props;
+    return <GridFormatComponent formats={formats} gameConfig={gameConfig} action={setGridFormat} />
   }
 
 }
 
-export default GridFormatContainer
+const matchDispatchToProps = dispatch => bindActionCreators(
+  { setGridFormat : actions.createSetGridFormatAction }, dispatch
+)
+
+
+export default connect(selectors.getGameConfig, matchDispatchToProps)(GridFormatContainer)
