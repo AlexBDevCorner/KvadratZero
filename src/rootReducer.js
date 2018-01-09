772810433e90d6ee7a7e86b67b1987d74e09cf2e
AleksB <aleksb@localhost.localdomain>
2018-01-09 20:22:32 +0200

    added gameConfig to rootReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,18 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-import playerListModule from './modules/player_list';
+import playerListModule from './modules/player_list'
+import gameConfigModule from './modules/game_config'
 
 const rootReducer = combineReducers({
-    playerList: playerListModule.reducer
-});
+    playerList : playerListModule.reducer,
+    gameConfig : gameConfigModule.reducer
+})
 
-const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  {},
+  applyMiddleware(thunk)
+)
 
-export default store;
+export default store
