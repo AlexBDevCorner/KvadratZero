d874b3adaf55b5adfab79c4f1663795412b14b5c
AleksB <aleksb@localhost.localdomain>
2018-01-10 11:45:30 +0200

    started with question grid module

---
+++ src/modules/question_grid/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_QUESTIONS = 'question_grid::add_questions'
+export const EDIT_QUESTION = 'question_grid::edit_question'
+
+export const ADD_THEMES = 'question_grid::add_themes'
+export const EDIT_THEME = 'question_grid::edit_themes'
---
+++ src/modules/question_grid/tests/actions.test.js
@@ -0,0 +1,78 @@
+import * as actionTypes from '../actionTypes'
+import * as actions from '../actions'
+
+describe('question grid actions tests', () => {
+
+  describe('ADD_QUESTIONS action tests', () => {
+
+    it('should create simple ADD_QUESTIONS action', () => {
+
+      const expectedAction = {
+        type : actionTypes.ADD_QUESTIONS,
+        questionCount : 8
+      }
+
+      expect(actions.createAddQuestionsAction(8)).toEqual(expectedAction)
+
+    })
+
+  })
+
+  describe('EDIT_QUESTION action tests', () => {
+
+    it('should create simple EDIT_QUESTION action', () => {
+
+      const question = {
+        id : 1,
+        text : "aa",
+        events : "",
+        points : 0
+      }
+
+      const expectedAction = {
+        type : actionTypes.EDIT_QUESTION,
+        question
+      }
+
+      expect(actions.createEditQuestionAction(question)).toEqual(expectedAction)
+
+    })
+
+  })
+
+  describe('ADD_THEMES action tests', () => {
+
+    it('should create simple ADD_THEMES action', () => {
+
+      const expectedAction = {
+        type : actionTypes.ADD_THEMES,
+        themesAmount : 8
+      }
+
+      expect(actions.createAddThemesAction(8)).toEqual(expectedAction)
+
+    })
+
+  })
+
+  describe('EDIT_THEME action tests', () => {
+
+    it('should create simple EDIT_THEME action', () => {
+
+      const theme = {
+        id : 1,
+        text : "text"
+      }
+
+      const expectedAction = {
+        type : actionTypes.EDIT_THEME,
+        theme
+      }
+
+      expect(actions.createEditThemesAction(theme)).toEqual(expectedAction)
+
+    })
+
+  })
+
+})
